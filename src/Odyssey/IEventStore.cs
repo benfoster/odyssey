21fd354333a4beab5bd959f04ee6752482a6fe3d
Ben Foster <ben@planetcloud.co.uk>
2022-11-03 09:53:11 +0000

    Added initialize to the event store interface

--- src/Odyssey/IEventStore.cs
+++ src/Odyssey/IEventStore.cs
@@ -2,6 +2,7 @@ namespace Odyssey;
 
 public interface IEventStore
 {
+    Task Initialize(CancellationToken cancellationToken);
     Task AppendToStream(string streamId, IReadOnlyList<EventData> events, StreamState expectedState, CancellationToken cancellationToken);
     Task<IReadOnlyCollection<EventData>> ReadStream(string streamId, ReadDirection direction, StreamPosition position, CancellationToken cancellationToken);
 }
